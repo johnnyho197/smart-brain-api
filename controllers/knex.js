68bcedbce15a6bd426ba9d8a478f7d846ca53413
Huu Ho <huu.ho@sjsu.edu>
2023-10-05 00:20:47 -0700

    updated pg

--- controllers/knex.js
+++ controllers/knex.js
@@ -3,11 +3,11 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'dpg-ckf64puafg7c7383d500-a',
-    user : 'smart_brain_sz29_user',
+    host : '127.0.0.1',
+    user : 'postgres',
     port : 5432,
-    password : 'pxTHFNN4rjNad4n8NPb0fJszM41n51iZ',
-    database : 'smart_brain_sz29'
+    password : 'rainwind171',
+    database : 'smart-brain'
   }
 });
 
